da9f802dde412f8719d46c9fb651ed553877459e
Loos Vincent <scur1152@int5.local.snellius.surf.nl>
2025-06-19 19:34:57 +0200

    relative paths for images in the database

--- app/modules/prompt_image.py
+++ app/modules/prompt_image.py
@@ -2,14 +2,11 @@ from PIL import Image
 import uuid
 import os
 
-def get_absolute_path(relative_path):
+def get_relative_path():
     """
-    Converts a relative path wrt to the file to an absolute path.
+    Returns a relative path for storing images.
     """
-    dirname = os.path.dirname(__file__)
-    absolute_path = os.path.join(dirname, relative_path)
-
-    return absolute_path
+    return os.path.join("images")
 
 class PromptImage():
     def __init__(self, image: Image.Image, prompt_guidance: float, image_guidance: float, input_prompt = None, output_prompt = None, save: bool = True):
@@ -18,14 +15,21 @@ class PromptImage():
         self.image : Image.Image = image
         self.prompt_guidance : float = prompt_guidance
         self.image_guidance : float = image_guidance
-        self.path : str = get_absolute_path("../../images/" + self.id + ".png")
+        
+        images_dir = get_relative_path()
+        self.path = os.path.join(images_dir, f"{self.id}.png")
         self.input_prompt = input_prompt
         self.output_prompt = output_prompt
 
         if save:
-            if not os.path.exists("images"):
-                os.makedirs("images")
+            if not os.path.exists(images_dir):
+                os.makedirs(images_dir)
+            
             self.image.save(self.path, format="PNG")
+
+    def get_path(self):
+        """Get the file path of the image."""
+        return self.path
 
     def set_input_prompt(self, prompt):
         """Set the input prompt for this image."""
