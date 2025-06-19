f700fbf3e8cf4e81bb329bb404274255dadc2f24
gilian <honkoopgilian@gmail.com>
2025-06-19 03:08:26 +0200

    changed database functions and example.ipynb

--- app/db/create.py
+++ app/db/create.py
@@ -21,8 +21,8 @@ CREATE TABLE IF NOT EXISTS chats (
 create_images_table = """
 CREATE TABLE IF NOT EXISTS images (
     id TEXT PRIMARY KEY,
-    input_prompt_id INTEGER NOT NULL,
-    output_prompt_id INTEGER NOT NULL,
+    input_prompt_id TEXT NOT NULL,
+    output_prompt_id TEXT NOT NULL,
     user_id INTEGER NOT NULL,
     chat_id INTEGER NOT NULL,
     prompt_guidance FLOAT NOT NULL,
